4818e7961fcb54e544755dd3df06f236be8e7588
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2024-04-17 16:53:37 +0530

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,5 +1,37 @@
 package Sorting;
 
+import java.util.Scanner;
+
 public class SelectionSort {
-    
+    static void Selection_Sort(int arr[],int n){
+        for (int i = 0; i <=n - 2; i++) {
+            int mini = i;
+            for (int j = i; j <= n-1; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Selection_Sort(arr,n);
+        sc.close();
+    }
 }
