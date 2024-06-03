69a1887e8bff23064838d3a743194ef21057dabe
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2024-06-03 17:25:40 +0530

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,37 +1,36 @@
 package Sorting;
-
-import java.util.Scanner;
-
 public class SelectionSort {
-    static void Selection_Sort(int arr[],int n){
-        for (int i = 0; i <=n - 2; i++) {
-            int mini = i;
-            for (int j = i; j <= n-1; j++) {
-                if (arr[j] < arr[mini]) {
-                    mini = j;
+    
+        static void selection_sort(int arr[], int n) {
+            for (int i = 0; i < n - 1; i++) {
+                int mini = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[mini]) {
+                        mini = j;
+                    }
                 }
+                //swap
+                int temp = arr[mini];
+                arr[mini] = arr[i];
+                arr[i] = temp;
             }
-            //swap
-            int temp = arr[mini];
-            arr[mini] = arr[i];
-            arr[i] = temp;
+    
+            System.out.println("After selection sort:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
         }
-
-        System.out.println("After selection sort:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+    
+        public static void main(String args[]) {
+    
+            int arr[] = {13, 46, 24, 52, 20, 9};
+            int n = arr.length;
+            System.out.println("Before selection sort:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            selection_sort(arr, n);
         }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-        Selection_Sort(arr,n);
-        sc.close();
-    }
 }
