2c9643123999dbcf0d3294166c02471f5692ee4b
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2024-08-19 10:33:24 +0530

    .

--- Array/LeftRotateTheArrayByDPlaces.java
+++ Array/LeftRotateTheArrayByDPlaces.java
@@ -1,5 +1,63 @@
 package Array;
 
+//Brute Force Approach
+/* 
 public class LeftRotateTheArrayByDPlaces {
-    
+static void leftRotateByD(int array[], int n, int k) {
+    int temp[] = new int[k];
+    for (int i = 0; i < k; i++) {
+      temp[i] = array[i];
+    }
+    //for Shifting the elements to left
+    for (int i = k; i < n; i++) {
+      array[i - k] = array[i];
+    }
+    //for adding the elements from temp array to the end of the array
+    for (int i = 0; i < k; i++) {
+      array[n - k + i] = temp[i];
+    }
+
+}
+
+public static void main(String args[]) {
+    int n = 7;
+    int[] arr = {1,2,3,4,5,6,7};
+    int k = 2;
+    leftRotateByD(arr, n, k);
+    System.out.println("After Rotating the elements to left ");
+    for (int i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");
+    }
+}
+}
+*/
+//Optimized Approach
+public class LeftRotateTheArrayByDPlaces {
+
+    public static void reverse(int array[], int start, int end) {
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void leftRotateByD(int array[], int n, int k) {
+        reverse(array, 0, k - 1);
+        reverse(array, k, n - 1);
+        reverse(array, 0, n - 1);
+    }
+
+    public static void main(String args[]) {
+        int n = 7;
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 2;
+        leftRotateByD(arr, n, k);
+        System.out.println("After Rotating the elements to left ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
---
+++ Array/MoveZeroesToEndOfArray.java
@@ -0,0 +1,46 @@
+package Array;
+import java.util.*;
+//Bruute Force Approach
+public class MoveZeroesToEndOfArray {
+    public static int[] moveZeros(int n, int a[]){
+        //temporary array:
+        ArrayList<Integer> temp = new ArrayList<>();
+        //copy non-zero elements
+        //from original -> temp array:
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0)
+                temp.add(a[i]);
+        }
+
+        // number of non-zero elements.
+        int nz = temp.size();
+
+        //copy elements from temp
+        //fill first nz fields of
+        //original array:
+        for (int i = 0; i < nz; i++) {
+            a[i] = temp.get(i);
+        }
+
+        //fill rest of the cells with 0:
+        for (int i = nz; i < n; i++) {
+            a[i] = 0;
+        }
+        return a;
+    }
+
+
+    public static void main(String[] args) {
+        int array[] = { 1, 0, 2, 0, 3, 0, 4, 5, 0, 6, 7, 0, 8, 9 };
+        int n = array.length;
+        int[] ans = moveZeros(n, array);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+
+
+
+}
+//Optimized Approach
