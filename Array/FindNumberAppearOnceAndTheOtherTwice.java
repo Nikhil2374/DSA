f58144c037fab1fd0826c9e88549bd9c06b266ed
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2024-09-09 18:01:06 +0530

    .

--- Array/FindNumberAppearOnceAndTheOtherTwice.java
+++ Array/FindNumberAppearOnceAndTheOtherTwice.java
@@ -34,9 +34,50 @@ public class FindNumberAppearOnceAndTheOtherTwice {
     }
 }
  */
+//Better approach 1:
+ //USing Hashing:
+ /* 
+ public class FindNumberAppearOnceAndTheOtherTwice {
+    public static int getSingleElement(int []arr) {
+        //size of the array:
+        int n = arr.length;
 
-//Better approach:
+        // Find the maximum element:
+        int maxi = arr[0];
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        // Declare hash array of size maxi+1
+        // And hash the given array:
+        int[] hash = new int[maxi + 1];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]]++;
+        }
+
+        //Find the single element and return the answer:
+        for (int i = 0; i < n; i++) {
+            if (hash[arr[i]] == 1)
+                return arr[i];
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
+ }
+
+*/
+//Better approach 2:
 //Using HashMap:
+/* 
 import java.util.*;
 
 public class FindNumberAppearOnceAndTheOtherTwice {
@@ -79,4 +120,28 @@ public class FindNumberAppearOnceAndTheOtherTwice {
     }
 
 
+}
+    */
+
+//Optimal approach:
+//Using XOR:
+public class FindNumberAppearOnceAndTheOtherTwice {
+    public static int getSingleElement(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // XOR all the elements:
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ arr[i];
+        }
+        return xorr;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
 }
