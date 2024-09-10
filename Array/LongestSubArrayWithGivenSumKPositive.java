785931cbda163455cbedadd5c63f49462bce20c2
Nikhil2374 <133630341+Nikhil2374@users.noreply.github.com>
2024-09-10 16:19:51 +0530

    Update LongestSubArrayWithGivenSumKPositive.java

--- Array/LongestSubArrayWithGivenSumKPositive.java
+++ Array/LongestSubArrayWithGivenSumKPositive.java
@@ -1,5 +1,30 @@
 package Array;
-
+//By using brute force approach
+//Appr
 public class LongestSubArrayWithGivenSumKPositive {
-    
+    public static int getLongestSubarray(int[] a, long k) {
+        int n = a.length;
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                long sum = 0;
+                for (int l = i; l <= j; l++) {
+                    sum += a[l];
+                }
+                if (sum == k) {
+                    len = Math.max(len, j - i + 1);
+                }
+            }
+        }
+        return len;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
 }
